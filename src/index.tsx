bc3464ee6dc29dbd35a4dfe7e30e1cf14ac17cf5
Shane Neubauer <shanen@gmail.com>
2024-02-19 17:39:29 +0000

    Add session refresh functionality to client lib

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 import Cookies from "js-cookie";
 
 const API_URL = "https://userstack.app/api/alpha";
+const DATA_TTL = 120000; // 2 minutes
 
 interface UserstackProviderProps {
   children: React.ReactNode;
@@ -29,16 +30,6 @@ export function UserstackProvider({ children, appId }: UserstackProviderProps) {
   const [currentPlan, setCurrentPlan] = useState("none");
   const [flags, setFlags] = useState([]);
 
-  useEffect(() => {
-    const session = Cookies.get(`_us_session`);
-    if (session) {
-      const sessionData = JSON.parse(session);
-      setSessionId(sessionData.sessionId);
-      setCurrentPlan(sessionData.plan);
-      setFlags(sessionData.flags);
-    }
-  }, []);
-
   const values = {
     identify: async (credential: string, { data }: { data: any }) => {
       const response = await fetch(`${API_URL}/identify`, {
@@ -55,7 +46,36 @@ export function UserstackProvider({ children, appId }: UserstackProviderProps) {
 
       if (response.ok) {
         const sessionData = await response.json();
-        Cookies.set(`_us_session`, JSON.stringify(sessionData), {
+        const cookie = {
+          time: new Date().getTime(),
+          ...sessionData,
+        };
+        Cookies.set(`_us_session`, JSON.stringify(cookie), {
+          expires: 36500, // 100 years should be enough
+        });
+      } else {
+        console.error("Failed to identify user");
+      }
+    },
+    refresh: async (sessionId: string) => {
+      const response = await fetch(`${API_URL}/refresh`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Userstack-App-Id": appId,
+        },
+        body: JSON.stringify({
+          sessionId,
+        }),
+      });
+
+      if (response.ok) {
+        const sessionData = await response.json();
+        const cookie = {
+          time: new Date().getTime(),
+          ...sessionData,
+        };
+        Cookies.set(`_us_session`, JSON.stringify(cookie), {
           expires: 36500, // 100 years should be enough
         });
       } else {
@@ -73,6 +93,23 @@ export function UserstackProvider({ children, appId }: UserstackProviderProps) {
     currentPlan,
   };
 
+  useEffect(() => {
+    const now = new Date().getTime();
+    const session = Cookies.get(`_us_session`);
+
+    if (session) {
+      const sessionData = JSON.parse(session);
+      if (sessionData.time + DATA_TTL > now) {
+        setSessionId(sessionData.sessionId);
+        setCurrentPlan(sessionData.plan);
+        setFlags(sessionData.flags);
+      } else {
+        // We passed the TTL, so we should refresh the session
+        values.refresh(sessionData.sessionId);
+      }
+    }
+  }, []);
+
   return (
     <UserstackContext.Provider value={values}>
       {children}
@@ -80,6 +117,6 @@ export function UserstackProvider({ children, appId }: UserstackProviderProps) {
   );
 }
 
-export default function useRstack() {
+export default function useUserstack() {
   return useContext(UserstackContext);
 }
